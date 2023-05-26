8b37811285b5b0afd47b1ebde9f461d33aa775c5
oLuccas <113742196+zLuiiix@users.noreply.github.com>
2023-05-25 21:11:11 -0300

    Create Portfolio.js

---
+++ Portfolio.js
@@ -0,0 +1,22 @@
+new TypeIt("#simpleUsage", {
+    speed: 50,
+    waitUntilVisible: true,
+    loop: true,
+})
+.pause(7500)
+.go();
+
+new TypeIt("#prog", {
+    speed: 50,
+    waitUntilVisible: true,
+    loop: true,
+})
+.pause(6000)
+.go();
+new TypeIt("#lorem", {
+    speed: 50,
+    waitUntilVisible: true,
+    loop: true,
+})
+.pause(7500)
+.go();
